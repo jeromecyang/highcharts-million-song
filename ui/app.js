16651b41c1e65c3604574e43db350b952196163a
Jerome Yang <jerome.yang@altisource.com>
2016-05-05 10:07:42 -0400

    render the data with highcharts

--- ui/app.js
+++ ui/app.js
@@ -11,7 +11,29 @@ app.directive("barChart", ["$http", function($http){
     link: function(scope, elem){
       $http.get('http://localhost:8080/aggregate/mean,tempo,year').then(function(response) {
         var chartData = response.data;
-        console.log(chartData);
+        var categories = chartData.map(function(d){ return d.label });
+        var values = chartData.map(function(d){ return d.value });
+        
+        elem.highcharts({
+          title: {
+            text: 'Tempo Over Years'
+          },
+          xAxis: {
+            categories: categories
+          },
+          yAxis: {
+            title: {
+              text: 'Tempo'
+            }
+          },
+          series: [{
+            name: 'Tempo',
+            data: values
+          }],
+          credits: {
+            enabled: false
+          }
+        });
       });
     }
   }
