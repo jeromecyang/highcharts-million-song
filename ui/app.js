994e5e6132bd960cbd6fc3bb1b143876f6e3189e
Jerome Yang <jerome.yang@altisource.com>
2016-05-05 09:50:49 -0400

    add a bar-chart directive

--- ui/app.js
+++ ui/app.js
@@ -1,4 +1,15 @@
 var app = angular.module("highchartsMillionSong", []);
+
 app.controller("mainController", ["$scope", function($scope){
   
+}]);
+
+app.directive("barChart", ["$http", function($http){
+  return {
+    restrict: "A",
+    scope: { chartDataBindTo: "=" },
+    link: function(scope, elem){
+      
+    }
+  }
 }]);
