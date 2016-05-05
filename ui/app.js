93005a85da39e8de1159bb399725a53c36783e2e
Jerome Yang <jerome.yang@altisource.com>
2016-05-05 10:07:07 -0400

    make HTTP request to obtain data from server

--- ui/app.js
+++ ui/app.js
@@ -9,7 +9,10 @@ app.directive("barChart", ["$http", function($http){
     restrict: "A",
     scope: { chartDataBindTo: "=" },
     link: function(scope, elem){
-      
+      $http.get('http://localhost:8080/aggregate/mean,tempo,year').then(function(response) {
+        var chartData = response.data;
+        console.log(chartData);
+      });
     }
   }
 }]);
